30bdfcc0bb503d98e024d1daead0946dcc0f4ea4
vishalkumar27440 <vishalkumar27440@gmail.com>
2026-07-23 09:07:11 +0530

    main.py

--- main.py
+++ main.py
@@ -768,7 +768,7 @@ async def upload(bot: Client, m: Message):
                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
                        count += 1
                        os.remove(f'{name}.pdf')
-                     except FloodWait as e:
+                    except FloodWait as e:
                        await m.reply_text(str(e))
                        time.sleep(e.x)
                        continue
